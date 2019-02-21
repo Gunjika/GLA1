e3081592897d80b5044f22f928c04915c9340cd5
Gunjika <gunjikavrshn23@gmail.com>
2019-02-21 11:14:09 +0530

    update java code

--- app/src/main/java/gla/gla1/MainActivity.java
+++ app/src/main/java/gla/gla1/MainActivity.java
@@ -22,8 +22,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Snackbar.make(view, "Gunjika", Snackbar.LENGTH_LONG)
+                        .setAction("OK", null).show();
             }
         });
     }
